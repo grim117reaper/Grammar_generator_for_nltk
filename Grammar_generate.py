a7ef75442f3aabcb60ddea473c260d5c030cf143
Aman Saxena <amanscgo@gmail.com>
2019-02-11 14:12:07 +0530

    added comments
    need to fix 2d list definition

--- Grammar_generate.py
+++ Grammar_generate.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
+
+#imports
 import os
 import nltk
 
+#list of example phrases
 sentences = ["loss of hair","bad mayocardial infractions"]
 
+#tokenizing
 token = []
 for i in sentences:
     token.append(nltk.word_tokenize(i))
 
+#tagging
 tagged = []
 for i in token:
     tagged.append(nltk.pos_tag(i))
 
+#finding different parts of speech
 k = 0
 l = [[],[],[],[],[],[]]
 for i in tagged:
@@ -19,17 +25,19 @@ for i in tagged:
         l[k].append(j[1])
     k = k + 1
 
+#finding unique parts of speech
 g = []
-
 for i in l:
     for j in i:
         if j not in g:
             g.append(j)
 
+#generating grammar parts
 gr = []
 for i in g:
     gr.append("<" + i + ">")
 
+#creatng the final grammar
 grammar = "NP: {("
 for i in gr:
     grammar = grammar + i + "*"
